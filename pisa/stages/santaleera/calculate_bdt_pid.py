c64cce2a4a3d84b8eed09ab59e41bfa70dc27ee6
Leander Fischer <leander.fischer@desy.de>
2019-06-20 14:06:14 +0200

    Now!:Changed calculate_bdt_pid stage so it uses ruglar python function now

--- pisa/stages/santaleera/calculate_bdt_pid.py
+++ pisa/stages/santaleera/calculate_bdt_pid.py
@@ -21,6 +21,8 @@ from sklearn.ensemble import GradientBoostingClassifier
 # Note that the filename as well as the class name should be all lower case
 # (the convention in Python is for class names to be UpperCamelCase).
 
+
+    
 class calculate_bdt_pid(PiStage):
     """
     Apply pre-trained BDT to reconstructed variables to calculate new pid
@@ -29,7 +31,6 @@ class calculate_bdt_pid(PiStage):
     ----------
     data
     params
-        bdt_length : name of the bdt chosen to calculate the pid
     input_names
     output_names
     debug_mode
@@ -53,7 +54,7 @@ class calculate_bdt_pid(PiStage):
                 ):
 
         # here we register our expected parameters so that PISA knows what to expect
-        expected_params = ('bdt_length')
+        expected_params = ()
 
         # any in-/output names could be specified here, but we won't need that for now
         input_names = ()
@@ -69,11 +70,6 @@ class calculate_bdt_pid(PiStage):
         output_calc_keys = ('calculated_pid',)
         # what keys are added or altered for the outputs during apply
         output_apply_keys = ('pid',)
-
-        # Load gbc version selected by bdt_length parameter:
-        bdt_path = os.path.join(os.environ['PISA_RESOURCES'], 'data/oscnext_santa_bdts/default_20_meters/alg.pckl')
-        global bdt 
-        bdt = pickle.load(open(bdt_path,'rb'))
 
         # init base class
         super(calculate_bdt_pid, self).__init__(data=data,
@@ -100,6 +96,11 @@ class calculate_bdt_pid(PiStage):
         # in case we need to initialize sth, like reading in an external file,
         # or add variables to the data object that we can later populate
         
+        # Load gbc version:
+        bdt_path = os.path.join(os.environ['PISA_RESOURCES'], 'data/oscnext_santa_bdts/default_25_meters/alg.pckl')
+        global bdt
+        bdt = pickle.load(open(bdt_path,'rb'))
+
         # do that in the right representation
         self.data.data_specs = self.calc_specs
         for container in self.data:
@@ -111,22 +112,18 @@ class calculate_bdt_pid(PiStage):
         # this function is called when parameters of this stage are changed (and the first time the
         # pipeline is run). Otherwise it is skipped.
 
-        bdt_length = self.params.bdt_length.m_as('dimensionless')
-
         for container in self.data:
             # the `.get(WHERE)` statements are necessary for numba to know if these arrays should be read from the host (CPU)
             # or the device (GPU).
             # No worries, this will work without a GPU too
-            calculate_pid(bdt_length,
-                          container['pid'].get(WHERE),
-                          container['leera_pid'].get(WHERE),
-                          container['track_reco'].get(WHERE),
-                          container['rho36_start_reco'].get(WHERE),
-                          container['z_start_reco'].get(WHERE),
-                          container['rho36_end_reco'].get(WHERE),
-                          container['z_end_reco'].get(WHERE),
-                          out=container['calculated_pid'].get(WHERE))
-            container['calculated_pid'].mark_changed(WHERE)
+            container['calculated_pid'] = self.calculate_pid(container['pid'].get(WHERE),
+                                                             container['leera_pid'].get(WHERE),
+                                                             container['track_reco'].get(WHERE),
+                                                             container['rho36_start_reco'].get(WHERE),
+                                                             container['z_start_reco'].get(WHERE),
+                                                             container['rho36_end_reco'].get(WHERE),
+                                                             container['z_end_reco'].get(WHERE),
+                                                            )
 
     def apply_function(self):
         # this function is called everytime the pipeline is run, so here we can just apply our factors
@@ -136,62 +133,28 @@ class calculate_bdt_pid(PiStage):
             # set the pid value to the calculated one
             vectorizer.set(container['calculated_pid'], out=container['pid'])
 
+    def calculate_pid(pid, leera_pid, track_reco, rho36_start_reco, z_start_reco, rho36_end_reco, z_end_reco, out):
+        """This function uses the pre-trained bdt to apply it to the events features
 
-# we will write a vectorized function here that works on both the CPU and the GPU,
-# and in single and double precision using Numba's `guvectorize`:
-# https://numba.pydata.org/numba-doc/dev/reference/jit-compilation.html#numba.guvectorize
+        Parameters
+        ----------
+        pid        : scalar
+                chi^2 ratio (track/cascade)
+        leera_pid        : scalar
+                llh difference (track-cascade)
+        track_reco       : scalar
+                reconstructed tracklength
+        rho36_start_reco : scalar
+                xy distance to string 36 (starting point)
+        rho36_end_reco   : scalar
+                xy distance to string 36 (end point)
+        z_start_reco     : scalar
+                depth (starting point)
+        z_end_reco       : scalar
+                depth (end point)
+        """
 
-# For numba to know what to do, we need to define the data types
-# of the arguments. These are called "signatures".
-
-# -> Put the most specific (lowest precision, unsigned, etc.) types before
-#    less specific types (otherwise the lower precision types will be upcast
-#    and performance will suffer for those types)
-
-# -> Use vector notation for signatures' datatypes, even if values will be
-#    scalars (you specify shapes in `layout`)
-
-signatures = [
-    '(f4[:], f4[:], f4[:], f4[:], f4[:], f4[:], f4[:], f4[:], f4[:])',
-    '(f8[:], f8[:], f8[:], f8[:], f8[:], f8[:], f8[:], f8[:], f8[:])'
-]
-
-# `layout` dictates the _shape_ of the arguments.
-# Here, all are scalars: (), and "->()" indicates the last arg is a scalar output which stores the result.
-# See https://numba.pydata.org/numba-doc/dev/reference/jit-compilation.html#numba.guvectorize
-
-layout = '(),(),(),(),(),(),(),()->()'
-
-@guvectorize(signatures, layout, target=TARGET)
-def calculate_pid(bdt_length, pid, leera_pid, track_reco, rho36_start_reco, z_start_reco, rho36_end_reco, z_end_reco, out):
-    """This function loads the pre-trained bdt and applies it to the events features/
-    
-    Parameters
-    ----------
-    pid        : scalar
-            chi^2 ratio (track/cascade)
-    leera_pid        : scalar
-            llh difference (track-cascade)
-    track_reco       : scalar
-            reconstructed tracklength
-    rho36_start_reco : scalar
-            xy distance to string 36 (starting point)
-    rho36_end_reco   : scalar
-            xy distance to string 36 (end point)
-    z_start_reco     : scalar
-            depth (starting point)
-    z_end_reco       : scalar
-            depth (end point)
-    out              : scalar
-            result of foo-ing and bar-ing the neutrino is stored here, _not_ returned
-    """
-    # Note that you have to "dereference" scalar arguments as if they are actually arrays
-
-#     # Load gbc version selected by bdt_length parameter:
-#     bdt_path = os.path.join(os.environ['PISA_RESOURCES'], 'data/oscnext_santa_bdts/', 'default_20_meters'.format(bdt_length), 'alg.pckl')
-#     bdt = pickle.load(open(bdt_path,'rb'))
-
-    feature_list = [pid[0], leera_pid[0], track_reco[0], rho36_start_reco[0], 
-                    z_start_reco[0], rho36_end_reco[0], z_end_reco[0]]
-    feature_array = np.array(feature_list).reshape(1,-1)
-    out[0] = bdt.predict_proba(feature_array)[:,1]
+        feature_list = [pid[0], leera_pid[0], track_reco[0], rho36_start_reco[0], 
+                        z_start_reco[0], rho36_end_reco[0], z_end_reco[0]]
+        feature_array = np.ndarray(feature_list).reshape(1,-1)
+        out[0] = bdt.predict_proba(feature_array)[:,1]
